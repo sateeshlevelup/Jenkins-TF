074f466d4db60d513b7141617fdeffe25ebbdfc5
sateeshlevelup <112386309+sateeshlevelup@users.noreply.github.com>
2023-04-21 12:26:21 +0530

    TF code to Create EC2 instance

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+ami           = "ami-06e46074ae430fba6" 
+instance_type = "t2.micro"
+tags = {
+    Name = "TF-Instance"
+}
+}
