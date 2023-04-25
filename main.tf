50c98293e52bd5d84779c5e6176dbd41f0e797b2
Ashok <cashok24@outlook.com>
2023-04-25 15:35:31 +0530

    bucket

--- main.tf
+++ main.tf
@@ -8,4 +8,16 @@ instance_type = "t2.micro"
 tags = {
     Name = "TF-Instance1"
 }
+provider "aws" {
+  region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "S3 Bucket" {
+  bucket = "sachinbuc8"
+
+  tags = {
+    Name        = "terrabuc"
+    }
+}
+}
+
